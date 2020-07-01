403b5e41ee2c18998965208bb2244da9996b757a
GofranChang <zhanggaofan0827@gmail.com>
2020-07-02 01:01:15 +0800

    [feature] 优化录音代码

--- device/AudioCapture.cpp
+++ device/AudioCapture.cpp
@@ -3,6 +3,60 @@
 #include <string.h>
 // #include "MyLogger.h"
 
+int initDevice(AVFormatContext** fmtCtx) {
+  avdevice_register_all();
+  
+//  AVFormatContext* fmtCtx = NULL;
+  char errors[1024] = {0};
+  
+  int ret = avformat_open_input(fmtCtx, ":0", av_find_input_format("avfoundation"), NULL);
+  if (ret < 0) {
+    av_strerror(ret, errors, 1024);
+    LOGE("device", "Open microphone error, error code {}, err info {}", ret, errors);
+    return -1;
+  }
+  
+  return 0;
+}
+
+int recordAudio(AVFormatContext* fmtCtx, AVPacket* pkt) {
+  return av_read_frame(fmtCtx, pkt);
+}
+
+int main(int argc, const char* argv[]) {
+  auto logger = my_media::KooLogger::Instance();
+  logger->initLogger(spdlog::level::debug, true, "", false);
+
+  av_log_set_level(AV_LOG_DEBUG);
+  
+  AVFormatContext* fmtCtx = NULL;
+  
+  int ret = 0;
+  ret = initDevice(&fmtCtx);
+  
+  AVPacket pkt;
+  av_init_packet(&pkt);
+  
+  FILE* fOut = fopen("/Users/gofran/Documents/workspace/gitproj/FfmpegDemo/resource/out.pcm", "wb+");
+  for (int i = 0; i < 500; i++) {
+    if (recordAudio(fmtCtx, &pkt) < 0) {
+      LOGE("device", "Record failed");
+      return -1;
+    } else {
+      LOGD("device", "Read ({}) pkt size {}", i, pkt.size);
+      fwrite(pkt.data, 1, pkt.size, fOut);
+      av_packet_unref(&pkt);
+    }
+  }
+  
+  fclose(fOut);
+  avformat_close_input(&fmtCtx);
+  LOGI("device", "Return 0");
+  
+  return ret;
+}
+
+#if 0
 int audioRecord() {
   auto logger = my_media::KooLogger::Instance();
   logger->initLogger(spdlog::level::debug, true, "", false);
@@ -36,7 +90,7 @@ int audioRecord() {
   fOutput = fopen(outPutPath, "wb+");
   
   SwrContext *swrCtx = NULL;
-  swrCtx = swr_alloc_set_opts(NULL, AV_CH_LAYOUT_STEREO, AV_SAMPLE_FMT_S16, 44100, AV_CH_LAYOUT_STEREO, AV_SAMPLE_FMT_FLT, 44100, 0, NULL);
+  swrCtx = swr_alloc_set_opts(NULL, AV_CH_LAYOUT_STEREO, AV_SAMPLE_FMT_S16, 44100, AV_CH_LAYOUT_MONO, AV_SAMPLE_FMT_FLT, 48000, 0, NULL);
   
   if (NULL == swrCtx) {
     LOGE("Main", "Alloc swr context error");
@@ -81,9 +135,26 @@ int audioRecord() {
   return 0;
 }
 
+int encodeAudio() {
+  int ret = 0;
+  
+//  avcodec_find_encoder_by_name("libfdk_aac");
+  AVCodec* codec = avcodec_find_encoder(AV_CODEC_ID_AAC);
+  AVCodecContext* codecCtx = avcodec_alloc_context3(codec);
+  codecCtx->sample_fmt = AV_SAMPLE_FMT_FLT;
+  codecCtx->channel_layout = AV_CH_LAYOUT_MONO;
+  codecCtx->channels = 1;
+  codecCtx->sample_rate = 48000;
+  codecCtx->bit_rate = 0;
+  codecCtx->profile = FF_PROFILE_AAC_HE_V2;
+  
+  return ret;
+}
+
 int main(int argc, const char* argv[]) {
   int ret = 0;
   
   ret = audioRecord();
   return ret;
 }
+#endif
