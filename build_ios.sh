de6de10c5ab32fddc82e07f18bb0411726ee4e8f
GofranChang <zhanggaofan0827@gmail.com>
2020-07-31 22:49:56 +0800

    [chore] Fix ios framework

--- build_ios.sh
+++ build_ios.sh
@@ -7,6 +7,4 @@ cmake ../ \
  -DCMAKE_TOOLCHAIN_FILE=../deps/iOS/ios.toolchain.cmake \
  -DPLATFORM=OS \
  -DIOS_PLATFORM=OS \
- -DBUILD_ARM64=1 \
- -DCMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/ \
- -DCMAKE_IOS_SDK_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/
+ -DCMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/
