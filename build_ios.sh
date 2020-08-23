80e4f4a18df46a573ffa280c72c9fec2907ab071
GofranChang <zhanggaofan0827@gmail.com>
2020-08-24 01:33:55 +0800

    [bugfix] Fix cmake warning

--- build_ios.sh
+++ build_ios.sh
@@ -5,9 +5,11 @@ cd build_ios
 cmake ../ \
  -G Xcode \
  -DCMAKE_TOOLCHAIN_FILE=../deps/iOS/ios.toolchain.cmake \
+ -DENABLE_BITCODE=false \
  -DPLATFORM=OS \
-#  -DIOS_PLATFORM=OS
- -DCMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/ \
- -DBUILD_PLATFORM=iOS \
- -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE
+ -DBUILD_PLATFORM=iOS
+
+#  -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE
 #  -DCMAKE_SKIP_RPATH=TRUE
+#  -DIOS_PLATFORM=OS
+#  -DCMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/
