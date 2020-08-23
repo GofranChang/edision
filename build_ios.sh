919f7653837d3e6bfc456482eb24162cf6a11ff2
GofranChang <zhanggaofan0827@gmail.com>
2020-08-24 00:49:36 +0800

    [chore] Modify ios cmake

--- build_ios.sh
+++ build_ios.sh
@@ -6,6 +6,8 @@ cmake ../ \
  -G Xcode \
  -DCMAKE_TOOLCHAIN_FILE=../deps/iOS/ios.toolchain.cmake \
  -DPLATFORM=OS \
- -DIOS_PLATFORM=OS \
+#  -DIOS_PLATFORM=OS
  -DCMAKE_IOS_DEVELOPER_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/ \
- -DBUILD_PLATFORM=iOS
+ -DBUILD_PLATFORM=iOS \
+ -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE
+#  -DCMAKE_SKIP_RPATH=TRUE
